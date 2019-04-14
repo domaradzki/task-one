6392143b4e3b8c77f66ce4f3fbf894bc90a10761
Grzegorz <domaradzki.g@gmail.com>
2019-04-14 18:09:06 +0200

    header styled, and menu on hover added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import person from '../../images/person_image.png';
+import arrow from '../../images/button_arrow.png';
 
 const Login = () => {
     return (
         <div className="login">
-            <div className="login__image">@img</div>
+            <div className="login__image"><img src={person} /></div>
             <div className="login__name">Olivia Wilde</div>
+            <div className="button__image"><img src={arrow} /></div>
           </div>
     )
 }
