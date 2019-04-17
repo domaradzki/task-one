d6c35429e0643f7a2fd319bec36604fedb9ce60c
Grzegorz <domaradzki.g@gmail.com>
2019-04-17 13:20:12 +0200

    changed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,15 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 
-gulp.task('reload', function() {
-    browserSync.reload();
-});
-
-gulp.task('serve', ['sass'], function() {
-    browserSync({
-        server: 'src'
-    });
-    gulp.watch('src/Components/**/*.js', ['reload']);
-    gulp.watch('src/scss/**/*scss',['sass'])
-});
-
 gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream());
+    gulp.src('src/scss/**/*.scss')
+ .pipe(sass().on('error', sass.logError))
+ .pipe(gulp.dest('src/css'));
+
 })
 
-gulp.task('default', ['serve']);
+gulp.task('sass:watch', function() {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch']);
